c459e8c1a409d6eebd357c0c0d44578dc9336215
SeijiNumata <seizi.numata@gmail.com>
2021-02-06 12:23:27 +0900

    レビュー反映１回目
    全てのレビューを反映し、テスト、Eslint済み

--- JScalender/calendar.js
+++ JScalender/calendar.js
@@ -15,40 +15,34 @@ if (argv.m) {
 } else {
   month = dateToday.getMonth()
 }
+console.log(`month=${month}`)
 
 // 日付を生成
-const dt = new Date(year, month, 2)
+const dt = new Date(year, month, 1)
+console.log(`${dt}`)
+
 const week = dt.getDay()
-
-// 月の最終日の日付取得
-function getLastDate (date) {
-  return new Date(date.getFullYear(), date.getMonth() + 1, 0)
-}
-const lastDate = getLastDate(dt)
-const lastDay = lastDate.getDate()
-
 // カレンダー出力
 console.log(`     ${year}年 ${month + 1}月`)
-console.log('月 火 水 木 金 土 日')
+console.log('日 月 火 水 木 金 土')
 
 // 最初の改行のスペースの数
-const firstBlank = week * 3 - 3
-if (firstBlank === -3) {
-  process.stdout.write('                  ')
-}
-for (let i = 0; i < firstBlank; i++) {
-  process.stdout.write(' ')
-};
+const blank = ('   ')
+process.stdout.write(blank.repeat(week))
 
-// 土曜日の改行の処理
-let saturdayNewLine = week
+// 日付をループしたいので、月の最終日の日付取得
+const lastDate = new Date(year, month + 1, 0)
+const lastDay = lastDate.getDate()
+
+// 日付出力
 for (let i = 1; i <= lastDay; i++) {
   const ljustDay = ('  ' + i).slice(-2)
   process.stdout.write(`${ljustDay} `)
-  // 土曜日がきたら改行
-  if (saturdayNewLine % 7 === 0) {
+
+  // 土曜日の改行の処理、曜日取得
+  const outputDayWeek = new Date(year, month, i).getDay()
+  if (outputDayWeek === 6) {
     console.log('')
   }
-  saturdayNewLine += 1
 }
 console.log('')
